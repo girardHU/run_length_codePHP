d54b5c980be9a977e842360ace1be6ebdfb22e69
GirardH <girardhugo77@gmail.com>
2019-07-22 10:01:51 +0200

    making decode_advanced_rle, not handling control char yet but looking good

--- rle.php
+++ rle.php
@@ -2,6 +2,7 @@
     $str_example1 = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW";
     $str_example2 = "12W1B12W3B24W1B14W";
     $str_example3 = "ffff ffff ffff 0909 ff4A 6BC0 D2ff ffff 0000 0000";
+    $str_example4 = "06 ff 02 09 00 05 ff 4A 6B C0 D2 03 ff 04 00";
 
     function encode_rle($str) {
         $tempo_char = 0;
@@ -42,6 +43,7 @@
 
     function encode_advanced_rle($str) {
         $str = str_replace(" ", "", $str);
+        // TODO gestion d'erreur complete
         if (!ctype_xdigit($str)) {
             echo "$$$";
             return "$$$";
@@ -56,7 +58,7 @@
             $first_couple = substr($str, $i, 2);
             $next_couple = substr($str, $i + 2, 2);
             $how_much = 1;
-            if ($first_couple == $next_couple) { // case 01
+            if ($first_couple == $next_couple) { // case same pattern
                 while ($first_couple == $next_couple) {
                     $how_much++;
                     $i += 2;
@@ -66,7 +68,7 @@
                 if ($how_much < 10)
                     $how_much = "0" . $how_much;
                 $result = $result . $how_much . " " . $first_couple. " ";
-            } else { // case 02
+            } else { // case unique pattern
                 while($first_couple != $next_couple) {
                     $how_much++;
                     $unique_patterns = $unique_patterns . " " . $first_couple;
@@ -82,6 +84,44 @@
             }
         }
         echo $result;
+        return $result;
+    }
+
+    function decode_advanced_rle($str) {
+        $str = str_replace(" ", "", $str);
+        // TODO gestion d'erreur complete
+        if (!ctype_alnum($str)) {
+            echo "$$$";
+            return "$$$";
+        }
+        $first_couple = "";
+        $next_couple = "";
+        $how_much = 0;
+        $result = "";
+
+        for ($i = 0; $i < strlen($str); $i += 4) {
+            $first_couple = substr($str, $i, 2);
+            $next_couple = substr($str, $i + 2, 2);
+            // echo $next_couple;
+            // echo "\n";
+            if (!ctype_digit($first_couple)) { // gestion err : pas un nombre
+                // echo $first_couple;
+                // echo "\n";
+                // echo gettype($first_couple);
+                echo "Houston, first_couple est pas un nombre\n";
+                return -1;
+            } else if ($first_couple == "00") { // character de controle
+                echo "j'ai just pas encore gerer le char de controle hehe\n";
+                return -1;
+            } else { // un nombre qui n'est pas 00
+                $result = $result . str_repeat($next_couple, $first_couple);
+                echo " resultat :" . $result;
+            }
+        }
+
+
+        echo $result;
+        return $result;
     }
 
     // function read_mbp_to_hex($path) {
@@ -90,6 +130,7 @@
 
     // encode_rle($str_example1);
     // decode_rle($str_example2);
-    encode_advanced_rle($str_example3);
+    // encode_advanced_rle($str_example3);
+    decode_advanced_rle($str_example4);
     // read_mbp_to_hex("./src/Super-Champignon.bmp");
 ?>
